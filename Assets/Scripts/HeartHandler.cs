e9432534e7b8298a219cd7ca9affa5922bf1dbf8
Isi171 <phil_93@hotmail.it>
2017-01-21 11:05:37 +0100

    Victory hearts spawn

---
+++ Assets/Scripts/HeartHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartHandler : MonoBehaviour
+{
+	public float totalDistance;
+	public float speed;
+	//distance per frame
+
+	private float currentDistance;
+
+	void Update()
+	{
+		currentDistance += speed;
+		Vector2 newPosition = transform.position;
+		newPosition.y += speed;
+		newPosition.x += Mathf.Sin(currentDistance);
+		transform.position = newPosition;
+		if (currentDistance > totalDistance)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
