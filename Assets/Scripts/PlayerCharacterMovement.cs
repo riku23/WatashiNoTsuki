494767f3b75f98a82d2854de64f60fd94f2aeaa7
Tommaso Celata <t.celata@gmail.com>
2017-01-21 10:27:43 +0100

    Boat

--- Assets/Scripts/PlayerCharacterMovement.cs
+++ Assets/Scripts/PlayerCharacterMovement.cs
@@ -9,28 +9,41 @@ public class PlayerCharacterMovement : MonoBehaviour
 	public Transform guyCollisionChecker2;
 	public LayerMask whatIsGround;
 	public LayerMask whatIsWater;
-	public BoxCollider2D onGroundBoxCollider;
+	public Collider2D onGroundBoxCollider;
 	public BoxCollider2D inWaterBoxCollider;
 
 	private float movementDirection;
 	private Rigidbody2D rigidbody2d;
 	private bool isOnGround;
 	private bool isInWater;
+    private GameObject boat;
+    private bool boated;
 
-	private void Start()
+    private void Start()
 	{
 		rigidbody2d = GetComponent<Rigidbody2D>();
+        boated = false;
 	}
 
 	private void Update()
 	{
 		movementDirection = Input.GetAxisRaw("Horizontal");
+        if (boated)
+        {
+            this.gameObject.transform.eulerAngles =  new Vector3 (boat.transform.rotation.z, 0,0);
+        }
+
 	}
 
 	private void FixedUpdate()
 	{
-		//Horizontal movement
-		isInWater = Physics2D.OverlapArea(guyCollisionChecker1.position, guyCollisionChecker2.position, whatIsWater);
+
+        if (boated)
+        {
+            this.gameObject.transform.rotation = boat.transform.rotation;
+        }
+        //Horizontal movement
+        isInWater = Physics2D.OverlapArea(guyCollisionChecker1.position, guyCollisionChecker2.position, whatIsWater);
 		if (!isInWater)
 		{
 			ActivateGroundCollider();
@@ -61,11 +74,24 @@ public class PlayerCharacterMovement : MonoBehaviour
 		inWaterBoxCollider.enabled = true;
 	}
     /*
-    void OnCollisionStay2D(Collision2D other)
+    private void OnCollisionStay2D(Collision2D other)
     {
         if(other.gameObject.tag == "Boat")
         {
-            this.gameObject.transform.parent = other.gameObject.transform;
+            Debug.Log("BOAT");
+            boated = true;
+            this.boat = other.gameObject;
         }
-    }*/
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Boat")
+        {
+            boated = false;
+            this.boat = null;
+            this.gameObject.transform.eulerAngles = Vector3.zero;
+        }
+    }
+    */
 }
