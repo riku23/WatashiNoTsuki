945f2b0aa1e89af892ca2d505b94c3fafbf30476
Tommaso Celata <t.celata@gmail.com>
2017-01-21 13:27:45 +0100

    menu background animated

---
+++ Assets/Scripts/MovingFisherScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingFisherScript : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform currentPoint;
+    public Transform[] points;
+    public int pointSelection;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentPoint = points[pointSelection];
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, currentPoint.position, Time.deltaTime * moveSpeed);
+        if (transform.position == currentPoint.position)
+        {
+            pointSelection++;
+
+            if (pointSelection == points.Length)
+            {
+                pointSelection = 0;
+            }
+
+            currentPoint = points[pointSelection];
+        }
+
+    }
+}
