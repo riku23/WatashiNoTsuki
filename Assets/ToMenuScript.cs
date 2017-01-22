ae491bcf9789f0cbb5a2cd2e8c4759623d2fdbf7
Marco Ballabio <marco.ballabio@outlook.it>
2017-01-22 01:52:44 +0100

    Added quit

---
+++ Assets/ToMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ToMenuScript : MonoBehaviour {
+	
+	// Door
+	GameObject door;
+	// Door script
+	BeginDoorScript doorScript;
+	// Has been exited?
+	bool exited = false;
+
+	// Use this for initialization
+	void Start () {
+		door = GameObject.Find("Door");
+		doorScript = door.GetComponent<BeginDoorScript>();	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!(Application.loadedLevelName == "Menu" || Application.loadedLevelName == "Credits") && (Input.GetKeyDown (KeyCode.JoystickButton7) || Input.GetKeyDown (KeyCode.Escape)) && !exited) {
+			exited = true;
+			StartCoroutine (LoadNext ());
+		}
+	}
+		
+	IEnumerator LoadNext()
+	{
+		yield return new WaitForSeconds (doorScript.SetOpen (false));
+		SceneManager.LoadScene ("Menu");
+	}
+
+}
