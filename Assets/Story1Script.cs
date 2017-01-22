1a2fce533944879aad7aaa3e21d4485688ae6217
Marco Ballabio <marco.ballabio@outlook.it>
2017-01-22 11:47:04 +0100

    Menu esc key

--- Assets/Story1Script.cs
+++ Assets/Story1Script.cs
@@ -8,19 +8,34 @@ public class Story1Script : MonoBehaviour {
 	GameObject door;
 	// Door script
 	BeginDoorScript doorScript;
+	// Is input enabled?
+	bool inputEnabled = false;
 
 	// Use this for initialization
 	void Start () {
 		door = GameObject.Find("Door");
 		doorScript = door.GetComponent<BeginDoorScript>();
-		StartCoroutine(LoadNext());
+		// Start the unlock coortutine
+		StartCoroutine(EnableInputDelayed());
+	}
+
+	void Update()
+	{
+		if (Input.anyKeyDown && inputEnabled)  {
+			inputEnabled = false;
+			StartCoroutine (LoadNext ());
+		}
 	}
 
 	IEnumerator LoadNext()
 	{
-		yield return new WaitForSeconds (6f);
 		yield return new WaitForSeconds (doorScript.SetOpen (false));
 		SceneManager.LoadScene ("Story2");
+	}
+
+	IEnumerator EnableInputDelayed() {
+		yield return new WaitForSeconds (doorScript.GetOpeningTime ());
+		inputEnabled = true;
 	}
 
 }
