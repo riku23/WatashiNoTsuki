67111a45d423b73cd4a5e7f9a94273b302bd8af1
Marco Ballabio <marco.ballabio@outlook.it>
2017-01-22 10:35:59 +0100

    Aggiunto Logo

---
+++ Assets/LogoScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LogoScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Disable the mouse
+		Cursor.visible = false;
+		StartCoroutine(LoadNext());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator LoadNext()
+	{
+		yield return new WaitForSeconds (1.5f);
+		SceneManager.LoadScene ("Menu");
+	}
+
+}
